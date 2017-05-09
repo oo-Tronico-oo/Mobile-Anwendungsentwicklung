1d4de4f0809a99d69434e058689c44f11ba53e06
oo-Tronico-oo <tronico@o2online.de>
2017-05-09 16:48:35 +0200

    implemented fade in/out

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@
 window.onload = initializeView;
 
 function initializeView() {
-    var body = document.getElementsByTagName("body");
+    var body = document.querySelector("body");
+    var main = document.querySelector("main");
     var viewButton = document.getElementsByClassName("view");
 
     viewButton[0].onclick = function() {
-        body[0].classList.toggle("tiles");
+        main.classList.toggle("faded");
+        main.addEventListener("transitionend", function toggleFade(){
+            main.classList.toggle("faded");
+            body.classList.toggle("tiles");
+            main.removeEventListener("transitionend", toggleFade);
+        });
     };
 }
