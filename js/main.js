536f7e54cee6cfbfa9d6db821fd3aecb3e3d8b61
oo-Tronico-oo <tronico@o2online.de>
2017-05-13 18:28:39 +0200

    implements dialog for list element and option element

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 /**
  * Created by Tronico on 27.04.2017.
  */
-window.onload = initializeView;
+window.onload = (function () {
+    var viewButton = document.getElementsByClassName("view")[0];
+    var ulElement = document.getElementsByTagName("ul")[0];
 
-function initializeView() {
-    var body = document.querySelector("body");
-    var main = document.querySelector("main");
-    var viewButton = document.getElementsByClassName("view");
+    // ****** change the view with faded in/out ************
+    viewButton.onclick = function() {
+        var body = document.getElementsByTagName("body")[0];
+        var main = document.getElementsByTagName("main")[0];
 
-    viewButton[0].onclick = function() {
         main.classList.toggle("faded");
         main.addEventListener("transitionend", function toggleFade(){
             main.classList.toggle("faded");
@@ -16,4 +17,37 @@ function initializeView() {
             main.removeEventListener("transitionend", toggleFade);
         });
     };
-}
+
+    /* click on list element -> open dialog
+     * content is title of list element
+     * if clicked item = menu of list element, content = title and url
+     */
+    ulElement.onclick = function(event){
+        var currentElement = event.target;
+        // is true, if you click menu button of list element
+        var isClickedElementMenu = currentElement.className.match("liElemMenu") !== null;
+        var liElement = getLiElement(currentElement);
+
+        if(liElement) {
+            var liElementTitle = liElement.getElementsByTagName("h2")[0].textContent;
+            var imageUrl = liElement.getElementsByClassName("liElemAutor")[0].textContent;
+
+            if (isClickedElementMenu) {
+                alert("Title: " + liElementTitle + "\nURL: " + imageUrl);
+            }
+            else {
+                alert("Title: " + liElementTitle);
+            }
+        }
+        // else{
+        //     alert("clicked element is not a list or list child");
+        // }
+    };
+
+    function getLiElement(elem){
+        if(elem.tagName === "UL")return null;
+        else if(elem.tagName === "LI")return elem;
+        else if(elem.parentNode) return getLiElement(elem.parentNode);
+        else return null;
+    }
+});
