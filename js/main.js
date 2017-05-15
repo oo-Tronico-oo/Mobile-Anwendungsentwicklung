cf95d319c0930a6539d172c690c9e44d4f3c7600
oo-Tronico-oo <tronico@o2online.de>
2017-05-15 15:14:49 +0200

    code review

--- js/main.js
+++ js/main.js
@@ -1,74 +1,101 @@
 /**
  * Created by Tronico on 27.04.2017.
  */
-window.onload = (function () {
-    var viewButton = document.getElementsByClassName("view")[0];
-    var ulElement = document.getElementsByTagName("ul")[0];
-    var addButton = document.getElementsByClassName("add")[0];
+// load js-file, if page ready
+window.onload = () => {
+    initEventHandler();
+};
 
-    /* change the view with faded in/out */
-    viewButton.onclick = function() {
-        var body = document.getElementsByTagName("body")[0];
-        var main = document.getElementsByTagName("main")[0];
+//############################## function ##############################################
 
+/**
+ * initilize Eventhandler
+ */
+function initEventHandler() {
+    document.getElementById("viewButton").onclick = viewButtonHandle;
+    document.getElementsByTagName("ul")[0].onclick = listElemHandle;
+    document.getElementById("addButton").onclick = addButtonHandle;
+}
+
+//############################### eventHandler ##########################################
+
+/**
+ * handler for change the view with faded in/out
+ *
+ * @param event is click event
+ */
+function viewButtonHandle(event) {
+    var body = document.getElementById("body");
+    var main = document.getElementById("main");
+
+    main.classList.toggle("faded");
+    main.addEventListener("transitionend", function toggleFade(){
         main.classList.toggle("faded");
-        main.addEventListener("transitionend", function toggleFade(){
-            main.classList.toggle("faded");
-            body.classList.toggle("tiles");
-            main.removeEventListener("transitionend", toggleFade);
-        });
-    };
+        body.classList.toggle("tiles");
+        main.removeEventListener("transitionend", toggleFade);
+    });
+}
 
-    /* click on list element -> open dialog
-     * content is title of list element
-     * if clicked item = menu of list element, content = title and url
-     */
-    ulElement.onclick = function(event){
-        var currentElement = event.target;
-        // is true, if you click menu button of list element
-        var isClickedElementMenu = currentElement.className.match("liElemMenu") !== null;
-        var liElement = getLiElement(currentElement);
+/**
+ * handler for click on list element (open a dialog)
+ * if clicked list element, content is title
+ * if clicked option menu of list element, content is title and image url
+ *
+ * @param event is click event
+ */
+function listElemHandle(event) {
+    var currentElement = event.target;
+    // is true, if you click option menu button of list element
+    // else false
+    var optionMenuIsClicked = currentElement.className.match("liElemMenu") !== null;
+    var liElement = getLiElement(currentElement);
 
-        if(liElement) {
-            var liElementTitle = liElement.getElementsByTagName("h2")[0].textContent;
-            var imageUrl = liElement.getElementsByClassName("liElemAutor")[0].textContent;
+    if(liElement) {
+        var liElementTitle = liElement.getElementsByTagName("h2")[0].textContent;
+        var imageUrl = liElement.getElementsByClassName("liElemAutor")[0].textContent;
 
-            if (isClickedElementMenu) {
-                alert("Title: " + liElementTitle + "\nURL: " + imageUrl);
-            }
-            else {
-                alert("Title: " + liElementTitle);
-            }
+        if (optionMenuIsClicked) {
+            alert("Title: " + liElementTitle + "\nURL: " + imageUrl);
         }
-        // else{
-        //     alert("clicked element is not a list or list child");
-        // }
-    };
-
-    /* add a new list element */
-    addButton.onclick = (event) => {
-        addItemToList({Title: "M6",
-                       Autor: "lorempixel.com",
-                        Date: "02.05.2017",
-                         src: "./img/lorempixel_200x100.jpg"});
-    };
-
-    function addItemToList(item){
-        var newListElement = ulElement.firstElementChild.cloneNode(true);
-
-        newListElement.getElementsByClassName("preview")[0].setAttribute("style", "background-image: url('" + item.src + "')");
-        newListElement.getElementsByTagName("h2")[0].textContent = item.Title;
-        newListElement.getElementsByClassName("liElemAutor")[0].textContent = item.Autor;
-        newListElement.getElementsByClassName("liElemDate")[0].textContent = item.Date;
-        newListElement.getElementsByClassName("liElemCount")[0].textContent = 0;
-
-        ulElement.appendChild(newListElement);
+        else {
+            alert("Title: " + liElementTitle);
+        }
     }
+}
 
-    function getLiElement(elem){
-        if(elem.tagName === "UL")return null;
-        else if(elem.tagName === "LI")return elem;
-        else if(elem.parentNode) return getLiElement(elem.parentNode);
-        else return null;
-    }
-});
+/**
+ * handler for append a new list element
+ *
+ * @param event is click event
+ */
+function addButtonHandle(event) {
+    var ulElement = document.getElementsByTagName("ul")[0];
+    var jasonItem = { Title: "M6",
+                      Autor: "lorempixel.com",
+                      Date: "02.05.2017",
+                      src: "./img/lorempixel_200x100.jpg" }
+
+    ulElement.appendChild(getNewListItem(jasonItem));
+}
+
+//################################## helper ###############################################
+
+function getLiElement(elem){
+    if(elem.tagName === "UL")return null;
+    else if(elem.tagName === "LI")return elem;
+    else if(elem.parentNode) return getLiElement(elem.parentNode);
+    else return null;
+}
+
+function getNewListItem(item){
+    var ulElement = document.getElementsByTagName("ul")[0];
+    var newListElement = ulElement.firstElementChild.cloneNode(true);
+
+    newListElement.getElementsByClassName("preview")[0].setAttribute("style", "background-image: url('" + item.src + "')");
+    newListElement.getElementsByTagName("h2")[0].textContent = item.Title;
+    newListElement.getElementsByClassName("liElemAutor")[0].textContent = item.Autor;
+    newListElement.getElementsByClassName("liElemDate")[0].textContent = item.Date;
+    newListElement.getElementsByClassName("liElemCount")[0].textContent = 0;
+
+    return newListElement;
+}
